a6f77331ea665751fc0f21a99828167ff03d8757
minchae0512 <jasma0512@hotmail.co.uk>
2016-09-25 22:36:33 +0200

    Player

--- Quize/magic.js
+++ Quize/magic.js
@@ -1,9 +1,30 @@
 $(document).ready(function(){
-  $('button').on('click', function(){
+
+  $('.inner button').on('click', function(){
     var klasa = $(this).attr('class');
     console.log(klasa);
     $('.card').removeClass('showing');
     $('.'+klasa).addClass('showing');
   });
+
+  var audioElement = document.createElement('audio');
+  audioElement.setAttribute('src', 'Yiruma.mp3');
+          //audioElement.load()
+          $.get();
+          audioElement.addEventListener("load", function() {
+          audioElement.play();
+          }, true);
+
+
+
+
+          $('.play').click(function() {
+          audioElement.play();
+          });
+
+
+          $('.pause').click(function(){
+            audioElement.pause();
+          });
 
 });
